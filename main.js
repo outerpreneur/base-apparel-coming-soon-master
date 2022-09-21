555044809a0c0d8e4c0f4f636c11466df0cbb624
Aleksander <alek@outerpreneur.com>
2022-09-21 19:33:50 +0200

    responsive and javascript

---
+++ main.js
@@ -0,0 +1,20 @@
+// ASSIGNING VARIABLES
+
+const submit = document.querySelector(".submit")
+const error = document.querySelector(".error")
+const form = document.querySelector("form")
+const input = docuemnt.querySelector("input")
+const label = docuemtn.querySelector("label")
+
+form.addEventListener("submit", validate)
+submitButton.addEventListener("click",validate)
+
+function validate(e) {
+    e.preventDefault()
+}
+
+// const validate = ( => {
+
+// })
+
+alert("hello motherfucker")
