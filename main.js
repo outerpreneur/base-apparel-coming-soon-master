c414bce19cf3f8d953a0087aa3916d370aa65afb
Aleksander <alek@outerpreneur.com>
2022-09-21 19:35:02 +0200

    responsive and javascript

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-// ASSIGNING VARIABLES
+// // ASSIGNING VARIABLES
 
 const submit = document.querySelector(".submit")
 const error = document.querySelector(".error")
 const form = document.querySelector("form")
-const input = docuemnt.querySelector("input")
-const label = docuemtn.querySelector("label")
+const input = document.querySelector("input")
+const label = document.querySelector("label")
+
+// VALIDATING SUBMISSIONS
 
 form.addEventListener("submit", validate)
-submitButton.addEventListener("click",validate)
+submit.addEventListener("click", validate)
 
 function validate(e) {
     e.preventDefault()
+
+// ASSIGNING INPUTVALUE FROM EMAIL ADDRESS VALUE 
+const inputValue = input.value.trim()
+
+
+// EMAIL ADDRESS CHECK
+// control flow showing different states depending on input
+if(!isEmail(inputValue)) {
+    error.style.display = "block";
+    label.style.display = "block";
+}  else {
+    error.style.display = "none";
+    label.style.display = "none";
 }
 
-// const validate = ( => {
+// check if is email with Regex
+function isEmail(input) {
+    return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(input)
+}
 
-// })
-
-alert("hello motherfucker")
+}
